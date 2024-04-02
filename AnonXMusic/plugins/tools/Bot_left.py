5aeae4b5c64c885329eec1ce1d68d8be5c91609d
ETZ-RAHUL <132138127+ETZ-RAHUL@users.noreply.github.com>
2024-04-02 19:20:10 +0530

    Update Bot_left.py

--- AnonXMusic/plugins/tools/Bot_left.py
+++ AnonXMusic/plugins/tools/Bot_left.py
@@ -3,9 +3,12 @@ from pyrogram import Client
 from pyrogram.types import Message
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto, InputMediaVideo, Message
-from config import LOGGER_ID as LOG_GROUP_ID
 from AnonXMusic import app  
 from AnonXMusic.utils.database import get_assistant
+
+LOG_GROUP_ID = -1001618846823
+LOGGER_ID = -1001618846823
+
 
 photo = [
     "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
@@ -21,12 +24,11 @@ async def on_left_chat_member(_, message: Message):
         userbot = await get_assistant(message.chat.id)
         
         left_chat_member = message.left_chat_member
-        if left_chat_member and left_chat_member.id == (await app.get_me()).id:
-            remove_by = message.from_user.mention if message.from_user else "𝐔ɴᴋɴᴏᴡɴ 𝐔sᴇʀ"
+        if left_chat_member and left_chat_member.id == (await app.get_me()).id: 
             title = message.chat.title
             username = f"@{message.chat.username}" if message.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐂ʜᴀᴛ"
             chat_id = message.chat.id
-            left = f"✫ <b><u>#𝐋ᴇғᴛ_𝐆ʀᴏᴜᴘ</u></b> ✫\n\n𝐂ʜᴀᴛ 𝐓ɪᴛʟᴇ : {title}\n\n𝐂ʜᴀᴛ 𝐈ᴅ : {chat_id}\n\n𝐑ᴇᴍᴏᴠᴇᴅ 𝐁ʏ : {remove_by}\n\n𝐁ᴏᴛ : @{app.username}"
+            left = f"✫ <b><u>#𝐋ᴇғᴛ_𝐆ʀᴏᴜᴘ</u></b> ✫\n\n𝐂ʜᴀᴛ 𝐓ɪᴛʟᴇ : {title}\n\n𝐂ʜᴀᴛ 𝐈ᴅ : {chat_id}\n\n𝐑ᴇᴍᴏᴠᴇᴅ 𝐁ʏ "
             await app.send_photo(LOG_GROUP_ID, photo=random.choice(photo), caption=left)
             
             await userbot.leave_chat(chat_id)
